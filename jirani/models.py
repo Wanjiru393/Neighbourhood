52ba0666868efa1bda48b6c262290f289da006bb
Wanjiru393 <carolciru393@gmail.com>
2022-06-19 14:34:02 +0300

    Created Profile Model

--- jirani/models.py
+++ jirani/models.py
@@ -5,6 +5,17 @@ from django.db.models import Q
 import datetime as dt
 
 
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to='avatars/')
+    description = HTMLField()
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    name =models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
 class neighbourhood(models.Model):
     neighbourhood= models.CharField(max_length=100)
 
