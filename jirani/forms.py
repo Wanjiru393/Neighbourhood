471ae040f96f5ef31d38df860b70def6c545fb97
Wanjiru393 <carolciru393@gmail.com>
2022-06-19 18:00:18 +0300

    Created Forms

--- jirani/forms.py
+++ jirani/forms.py
@@ -1,14 +1,18 @@
-from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from django.forms import ModelForm
-from django.contrib.auth.models import User
+from .models import notifications,Business,Profile
 
-class RegisterForm(UserCreationForm):
-    username = forms.CharField(max_length=50, label='username', widget=forms.TextInput(attrs={'class': 'form-control mb-3'}))
-    email = forms.EmailField(max_length=50, label='email', widget=forms.EmailInput(attrs={'class': 'form-control mb-3'}))
-    password1 = forms.CharField(max_length=50, label='password', widget=forms.PasswordInput(attrs={'class': 'form-control mb-3'}))
-    password2 = forms.CharField(max_length=50, label='confirm password', widget=forms.PasswordInput(attrs={'class': 'form-control mb-3'}))
 
+class notificationsForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        model=notifications
+        exclude=['author','neighbourhood','post_date']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=['username']
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model=Business
+        exclude=['owner','neighbourhood']
