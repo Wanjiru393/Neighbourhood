b3d9dcf36f5233f7818888e354e45b572ca75832
Wanjiru393 <carolciru393@gmail.com>
2022-06-19 15:12:01 +0300

    Registered models

--- jirani/admin.py
+++ jirani/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import neighbourhood,healthservices,Health,notifications,Authorities,Profile
+
+
+class HealthAdmin(admin.ModelAdmin):
+    filter_horizontal =['healthservices']
 
 # Register your models here.
+admin.site.register(neighbourhood)
+admin.site.register(Health,HealthAdmin)
+admin.site.register(healthservices)
+admin.site.register(Authorities)
+admin.site.register(Profile)
+admin.site.register(notifications)
