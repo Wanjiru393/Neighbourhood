59c21e45d32a043e39ac2c51110732ac081e2b7e
Wanjiru393 <carolciru393@gmail.com>
2022-06-19 14:29:42 +0300

    Created Neighbourhood model

--- jirani/models.py
+++ jirani/models.py
@@ -1,6 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from django.db.models import Q
+import datetime as dt
 
-# Create your models here.
+
+class neighbourhood(models.Model):
+    neighbourhood= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.neighbourhood
+
+    def save_neighbourhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood(cls,neighbourhood):
+        cls.objects.filter(neighbourhood=neighbourhood).delete()
 
 
 
