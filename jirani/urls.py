1538eb8c4ddb2bd73217e195665b480d086e6041
Wanjiru393 <carolciru393@gmail.com>
2022-06-20 09:01:08 +0300

    Added urls for new templates

--- jirani/urls.py
+++ jirani/urls.py
@@ -12,6 +12,11 @@ urlpatterns = [
     path('authorities',views.authorities, name='authorities'),
     path('businesses',views.businesses, name='businesses'),
     path('my-profile/',views.my_profile, name='my-profile'),
-    path('user/(?P<username>\w{0,50})',views.user_profile,name='user-profile'),
+    path('user/',views.user_profile,name='user-profile'),
+    path('new/business',views.new_business, name='new-business'),
+    path('create/profile',views.create_profile, name='create-profile'),
+    path('new/notification',views.new_notification, name='new-notification'),
+    path('update/profile',views.update_profile, name='update-profile'),
 ]
 
+# (?P<username>\w{0,50})
